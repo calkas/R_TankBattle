f56ebc4c30277d52c4edd8e42fbd5d6ca7548ded
Piotr Rybka <piotr.rybka@nokia.com>
2023-02-23 12:52:21 +0100

    R_TankBattle init creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate piston_window;
+use piston_window::*;
+
+
 fn main() {
     println!("..::R_TankBattle::..");
+
+    let window_res = [600.0, 400.0];
+    let mut windows_settings = WindowSettings::new("RTankBattle", window_res);
+    windows_settings.set_exit_on_esc(true);
+
+
+    let mut window: PistonWindow = windows_settings.build().unwrap();
+
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(& mut window)  {
+        
+    }
+
+
+
+    println!("...::END::..")
 }
